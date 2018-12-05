31572c1e409e4eeb8a06cf207c17298c96d069c3
Dominik Ehrenberg <doe@crosscan.com>
2018-12-05 10:02:45 +0100

    Revert "Revert "use forked package instead of original""
    This reverts commit 1984cc94db1bee78053d6eda9dee971ff1bbab7c.

--- manifests/package.pp
+++ manifests/package.pp
@@ -14,7 +14,11 @@ class phpldapadmin::package {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  package { $phpldapadmin::params::package_name:
-    ensure => installed,
-  }
+//  package { $phpldapadmin::params::package_name:
+//    ensure => installed,
+//  }
+
+  mod $phpldapadmin::params::package_name,
+    :git => 'https://github.com/breisig/phpLDAPadmin',
+    :install_path => '/usr/share/phpldapadmin'
 }
